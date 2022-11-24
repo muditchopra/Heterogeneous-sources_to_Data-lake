eb515db911d711914f2ec280729436c6299443ed
mudit <mudits138@gmail.com>
2022-11-24 17:01:36 +0530

    fix

--- terraform/test.sh
+++ terraform/test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 set -e
 
+ls -lrt /opt
+ls -lrt /usr/local/bin
+ls -lrt /usr/bin
 terraform --version
 aws --version
 dot -V
 pytest --version
 pylint --version
 python --version
-ls -lrt /opt
-ls -lrt /usr/local/bin
-ls -lrt /usr/bin
