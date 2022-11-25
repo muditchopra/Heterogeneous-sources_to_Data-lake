9ba483c29c4ba8e60c078bb2ce4ad91ac0e37acb
mudit <mudits138@gmail.com>
2022-11-25 12:40:11 +0530

    filter added

--- infra-setup/deploy-scripts/glue-jobs/glue-job-rds-data.py
+++ infra-setup/deploy-scripts/glue-jobs/glue-job-rds-data.py
@@ -24,22 +24,30 @@ Insurance = glueContext.create_dynamic_frame.from_catalog(
              database="tyropower-prod-Catalog-db",
              table_name="Insurance")
 
+print("Drop cloumns from credit card and loan")
 CreditCard = CreditCard.drop_fields(['Annual_Income'])
 LoanDetails = LoanDetails.drop_fields(['Annual_Income','Credit_Score','HomeOwnership'])
+print("Rename columns in loan table")
+LoanDetails = LoanDetails.apply_mapping(
+                                    [("Term","String","LoanTerm","String"),
+                                     ("Type","String","LoanType","String")]
+)
 
 
 
-
-
+print("convert string to int (Y&N)->(1&0)")
 CustomerDetails['SeniorCitizen'] = CustomerDetails['SeniorCitizen'].map(f= lambda t: 1 if t=='Y' else 0)
 CustomerDetails['Loan'] = CustomerDetails['Loan'].map(f= lambda t: 1 if t=='Y' else 0)
 CustomerDetails['Insurance'] = CustomerDetails['Insurance'].map(f= lambda t: 1 if t=='Y' else 0)
-
+print("joinning the dynamic data frames")
 Customer_Data = Join.apply(Insurance,
                                     Join.apply(Loan,
                                     Join.apply(credit_card,customer_details,'CardCustomerID','CustomerID'),
                                 'LoanCustomerID','CustomerID'),
                                 'Customer_ID','CustomerID').drop_fields(['CardCustomerID','LoanCustomerID','Customer_ID'])
-
-s_history = Customer_Data.toDF().repartition(1)
-s_history.write.parquet('s3://prod-tyropower-datalake-ap-south-1/silver/customer-details/')
+print("write the data to s3")
+glueContext.write_dynamic_frame.from_options(
+       frame = Customer_Data,
+       connection_type = "s3",
+       connection_options = {"path": "s3://prod-tyropower-datalake-ap-south-1/silver/customer-details"},
+       format = "parquet")
