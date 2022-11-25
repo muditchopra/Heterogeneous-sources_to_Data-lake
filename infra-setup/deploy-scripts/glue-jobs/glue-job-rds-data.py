b46b4b420166b370d31837860320005b12d4a1b2
mudit <mudits138@gmail.com>
2022-11-25 19:59:27 +0530

    job updated

--- infra-setup/deploy-scripts/glue-jobs/glue-job-rds-data.py
+++ infra-setup/deploy-scripts/glue-jobs/glue-job-rds-data.py
@@ -27,22 +27,27 @@ Insurance = glueContext.create_dynamic_frame.from_catalog(
 print("Drop cloumns from credit card and loan")
 CreditCard = CreditCard.drop_fields(['Annual_Income'])
 LoanDetails = LoanDetails.drop_fields(['Annual_Income','Credit_Score','HomeOwnership'])
+CreditCard.printSchema()
+
 print("Rename columns in loan table")
 LoanDetails = LoanDetails.apply_mapping(
                                     [("Term","String","LoanTerm","String"),
                                      ("Type","String","LoanType","String")]
 )
-
+LoanDetails.printSchema()
 print("convert string to int (Y&N)->(1&0)")
 CustomerDetails['SeniorCitizen'] = CustomerDetails['SeniorCitizen'].map(f= lambda t: 1 if t=='Y' else 0)
 CustomerDetails['Loan'] = CustomerDetails['Loan'].map(f= lambda t: 1 if t=='Y' else 0)
 CustomerDetails['Insurance'] = CustomerDetails['Insurance'].map(f= lambda t: 1 if t=='Y' else 0)
+CustomerDetails.printSchema()
 print("joinning the dynamic data frames")
 Customer_Data = Join.apply(Insurance,
                                     Join.apply(Loan,
                                     Join.apply(credit_card,customer_details,'CardCustomerID','CustomerID'),
                                 'LoanCustomerID','CustomerID'),
                                 'Customer_ID','CustomerID').drop_fields(['CardCustomerID','LoanCustomerID','Customer_ID'])
+
+Customer_Data.printSchema()
 print("write the data to s3")
 glueContext.write_dynamic_frame.from_options(
        frame = Customer_Data,
