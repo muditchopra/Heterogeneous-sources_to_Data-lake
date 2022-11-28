fe5907a01c6b652f4c0cc71d4722aa4f730e4542
mudit <mudits138@gmail.com>
2022-11-28 16:37:35 +0530

    updated

--- infra-setup/db/dbinit.sh
+++ infra-setup/db/dbinit.sh
@@ -4,8 +4,8 @@ MY_PASS_MASTER="abcdefghijklmnopqrstuvwxyz1234567890"
 MY_DBNAME_APP1=tyropower
 MY_USER_APP1=tyropower
 MY_PASS_APP1=0987654321zyxwvutsrqponmlkjihgfedcba
-MY_HOST1=`aws ssm get-parameter --with-decryption --name "/tyropower/${ENV}/mysql/host/reader" --query 'Parameter.Value'|sed -e 's/^"//' -e 's/"$//'|cut -d ':' -f1`
-MY_HOST=`echo $MY_HOST1| cut -d ':' -f1`
+MY_HOST=`aws ssm get-parameter --with-decryption --name "/tyropower/${ENV}/mysql/host/reader" --query 'Parameter.Value'|sed -e 's/^"//' -e 's/"$//'|cut -d ':' -f1`
+# MY_HOST=`echo $MY_HOST1| cut -d ':' -f1`
 # Install MySQL client
 apk add mysql mysql-client
 
