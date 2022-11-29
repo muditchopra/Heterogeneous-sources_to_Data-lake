d824f334753bcf7f96783dce9efcaf01a3972d75
mudit <mudits138@gmail.com>
2022-11-29 13:07:28 +0530

    db schema fixed

--- infra-setup/db/tyropower.sql
+++ infra-setup/db/tyropower.sql
@@ -16,8 +16,8 @@ CREATE TABLE `customer_details` (
   `Occupation` varchar(20) DEFAULT NULL,
   `AnnualIncome` float DEFAULT NULL,
   `Loan` varchar(20) DEFAULT NULL,
-  `Insurance` varchar DEFAULT NULL,
-  PRIMARY KEY (`Customer_ID`)
+  `Insurance` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`CustomerID`)
 );
 
 DROP TABLE IF EXISTS `credit_card`;
@@ -25,7 +25,7 @@ CREATE TABLE `credit_card` (
   `CardID` varchar(20) NOT NULL,
   `CardCustomerID` bigint(20) DEFAULT NULL,
   `CreditScore` int(11) DEFAULT NULL,
-  `AnnualIncome` float DEFAULT NULL,
+  `Annual_Income` float DEFAULT NULL,
   `YearofCreditHistory` float DEFAULT NULL,
   `CreditLimit` int DEFAULT NULL,
   `LastPendingAmt` float DEFAULT NULL, 
@@ -48,15 +48,15 @@ INSERT INTO customer_details (CustomerID, Gender, Age, Married, SeniorCitizen,
 (611310,'male',31,'married','N',9876543210,'abcxyz@gmail.com',151088,'Own Home','business',7,'N','Y'),
 (611311,'male',54,'married','N',9876543210,'abcxyz@gmail.com',212800,'Home Mortgage','job',6,'N','N'),
 (611312,'male',63,'married','Y',9876543210,'abcxyz@gmail.com',65474,'Rent','job',8,'N','N'),
-(611313,'female',24,'single','N',9876543210,'abcxyz@gmail.com',137332,'Own Home','student',,'Y','N'),
-(611314,'female',23,'single','N',9876543210,'abcxyz@gmail.com',281599,'Home Mortgage','student',,'N','Y'),
+(611313,'female',24,'single','N',9876543210,'abcxyz@gmail.com',137332,'Own Home','student',0,'Y','N'),
+(611314,'female',23,'single','N',9876543210,'abcxyz@gmail.com',281599,'Home Mortgage','student',0,'N','Y'),
 (611315,'female',45,'married','N',9876543210,'abcxyz@gmail.com',729695,'Own Home','job',9,'N','N'),
 (611316,'female',49,'married','N',9876543210,'abcxyz@gmail.com',164350,'Rent','business',7,'N','N'),
 (611317,'male',64,'divorced','Y',9876543210,'abcxyz@gmail.com',347396,'Own Home','job',5,'Y','N'),
 (611318,'female',64,'married','Y',9876543210,'abcxyz@gmail.com',330581,'Own Home','job',8,'N','N'),
 (611319,'male',57,'married','N',9876543210,'abcxyz@gmail.com',121467,'Home Mortgage','business',11,'Y','N'),
 (611320,'male',59,'divorced','N',9876543210,'abcxyz@gmail.com',125020,'Own Home','job',12,'N','Y'),
-(611321,'female',25,'single','N',9876543210,'abcxyz@gmail.com',174344,'Rent','student',,'N','N'),
+(611321,'female',25,'single','N',9876543210,'abcxyz@gmail.com',174344,'Rent','student',0,'N','N'),
 (611322,'female',37,'single','N',9876543210,'abcxyz@gmail.com',85633,'Own Home','job',9,'Y','N'),
 (611323,'male',29,'single','N',9876543210,'abcxyz@gmail.com',827051,'Rent','business',10,'N','N'),
 (611324,'male',28,'single','N',9876543210,'abcxyz@gmail.com',684950,'Rent','business',7,'N','N'),
@@ -83,5 +83,5 @@ INSERT INTO credit_card (CardID,CardCustomerID,CreditScore,Annual_Income,YearofC
 ('b171374b',611315,680,11,3.5,125000,1596,15905),
 ('401b284d',611325,696,8.5,4.9,150000,16511,32879),
 ('54459ce0',611308,703,11,3.6,100000,65474,145236),
-('6866ead5',611306,803,8,12,600000,318953,)
+('6866ead5',611306,803,8,12,600000,318953,388421)
 ;
