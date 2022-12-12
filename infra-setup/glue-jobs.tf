20ff3b19ba2d4a40206bb4dbc1b2caa984f45bf6
mudit <mudits138@gmail.com>
2022-12-12 21:09:08 +0530

    job fix

--- infra-setup/glue-jobs.tf
+++ infra-setup/glue-jobs.tf
@@ -7,6 +7,13 @@ resource "aws_glue_job" "job" {
     script_location = "s3://${lower(var.env)}-${lower(var.project_name)}-scripts-${var.aws_region}/scripts/glue-job-rds-data.py"
   }
   connections = [aws_glue_connection.jdbc-connection.id]
+  default_arguments = {
+    # ... potentially other arguments ...
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--TempDir"                          = "s3://prod-tyropower-scripts-us-east-1/temp"
+    "--job-language"                     = "python"
+    "--job-bookmark-option"              = "job-bookmark-disable"
+  }
 }
 
 resource "aws_glue_job" "job2" {
@@ -15,5 +22,12 @@ resource "aws_glue_job" "job2" {
 
   command {
     script_location = "s3://${lower(var.env)}-${lower(var.project_name)}-scripts-${var.aws_region}/scripts/data-load.py"
+  }
+  default_arguments = {
+    # ... potentially other arguments ...
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--TempDir"                          = "s3://prod-tyropower-scripts-us-east-1/temp"
+    "--job-language"                     = "python"
+    "--job-bookmark-option"              = "job-bookmark-disable"
   }
 }
